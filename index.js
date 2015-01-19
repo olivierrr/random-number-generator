b7008232e4fb7d2be8ccf2ab22a4fd4c6c07141f
olivierrr <oliviersinbox@gmail.com>
2015-01-18 19:18:00 -0800

    initial

---
+++ index.js
@@ -0,0 +1,36 @@
+
+var crypto = require('crypto')
+
+module.exports = random
+
+function random(max, min) {
+  min = min || 0
+
+  if(min >= max) {
+    return max
+  }
+
+  var range = (max - min) + 1
+    , bytes = 1
+
+  while(Math.pow(2, bytes * 8) <= range) {
+    bytes++
+  }
+
+  var cutoff = Math.floor(Math.pow(2, bytes * 8) / range) * range - 1
+    , buf    = null
+    , num    = cutoff + 1
+
+  while(num > cutoff) {
+    try {
+      buf = crypto.randomBytes(bytes)
+    } catch(err) {
+      buf = crypto.pseudoRandomBytes(bytes)
+    }
+    num = 0
+    for(var i = 0; i < bytes; i++) {
+      num += (buf.readUInt8(i) << 8 * (bytes - i - 1))
+    }
+  }
+  return (num % range) + min
+}
